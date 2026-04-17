55680b20e7d177ba56af1a40eee3822d82906182
eyad-99 <62146776+eyad-99@users.noreply.github.com>
2026-04-17 21:26:23 +0200

    basic producer-consumer

---
+++ RabbitMQ-test/Consumer/Program.cs
@@ -0,0 +1,26 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+using System;
+using System.Text;
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var Connection = factory.CreateConnection();
+using var channel = Connection.CreateModel();
+channel.QueueDeclare(
+    queue: "letterbox",
+    durable: false,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null
+    );
+var consumer= new EventingBasicConsumer(channel);
+consumer.Received += (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message= Encoding.UTF8.GetString(body);
+    Console.WriteLine("message received"+message);
+
+
+};
+channel.BasicConsume(queue: "letterbox", autoAck: true, consumer: consumer);
+Console.ReadKey();
---
+++ RabbitMQ-test/Producer/Program.cs
@@ -0,0 +1,17 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var Connection = factory.CreateConnection();
+using var channel = Connection.CreateModel();
+channel.QueueDeclare(
+    queue: "letterbox",
+    durable: false,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null
+    );
+var message = "this is my first message";
+var encodedmessage  = Encoding.UTF8.GetBytes(message);
+channel.BasicPublish("", "letterbox", null, encodedmessage);
+Console.WriteLine("published messagge    "+message);
